44c9009848cf42c3ebc6db3b3c7820a24dd8e3a9
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2025-03-08 02:12:32 +0000

    whaddya think?

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["5173-ogagaakpowe-mernstackre-9fg5uj019rw.ws-eu118.gitpod.io"],
+    host: true,  // Ensure Vite listens on the correct host
+    strictPort: true, // Prevents port conflicts
+  },
 })
