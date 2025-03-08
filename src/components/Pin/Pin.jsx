69bf9c368c99300f19d2e373dc7fe3c2d1fb1d54
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2025-03-08 02:35:47 +0000

    modified:   src/components/Pin/Pin.jsx

--- src/components/Pin/Pin.jsx
+++ src/components/Pin/Pin.jsx
@@ -11,7 +11,7 @@ function Pin({ item }) {
             <img src={item.img} alt="" />
             <div className="textContainer">
                 <Link to={`/${item.id}`}>{item.title}</Link>
-                <span>{item.bedroom}</span>
+                <span>{item.bedroom} bedroom</span>
                 <b>${item.price}</b>
             </div>
           </div>
